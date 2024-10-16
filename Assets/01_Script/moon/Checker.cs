7403519da7275ed493ca39ab6c50e37759072d27
moon080411 <anstjdalsdlek@gmail.com>
2024-10-16 23:38:11 +0900

    feat:EnemyDataSO

---
+++ Assets/01_Script/moon/Checker.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checker : MonoBehaviour
+{
+    public int findedEnemy;
+    public int FindedEnemy
+    {
+        get
+        {
+            return findedEnemy;
+        }
+        set
+        {
+            if(value <= 0)
+            {
+                findedEnemy = 0;
+                IsEnemy = false;
+            }
+            else
+            {
+                findedEnemy = value;
+                IsEnemy = true;
+            }
+        }
+    }
+    private bool isEnemy;
+    public bool IsEnemy
+    {
+        get
+        {
+            return isEnemy;
+        }
+        set
+        {
+            isEnemy = value;
+            if (isEnemy)
+            {
+                OnEnemyEnter?.Invoke();
+            }
+            else
+            {
+                OnEnemyAllExit?.Invoke();
+            }
+        }
+    }
+    public event Action OnEnemyEnter,OnEnemyAllExit;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindedEnemy++;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        FindedEnemy--;
+    }
+}
