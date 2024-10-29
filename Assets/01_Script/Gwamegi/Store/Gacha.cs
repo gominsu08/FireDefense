98d998a62ec91e4d2da7937dbb0fe21720f66478
gominsu08 <minsu080618@gmail.com>
2024-10-29 23:16:39 +0900

    feat : Tile

--- Assets/01_Script/Gwamegi/Store/Gacha.cs
+++ Assets/01_Script/Gwamegi/Store/Gacha.cs
@@ -1,20 +1,29 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gacha : MonoBehaviour
 {
-    [SerializeField] private MassegeBox MassegeWindow;
+    [SerializeField] private MassegeBox _massegeWindow;
     [SerializeField] private GachaManager _manager;
     /// <summary>
     /// 한번 가챠에 필요한 재화의 양
     /// </summary>
     [SerializeField] private int _tryCoinCount;
+    [SerializeField] private ChoiseItem _choiseItem;
     private UnitDataList _unitDataList;
+
+    public event Action OnStartGachaEvent;
+    public event Action<List<UnitDataSO>> OnChoiseUnitCheckEvent;
+    public event Action OnChoiseUnitItemResetEvent;
 
     private void Start()
     {
         _unitDataList = _manager.unitDataList;
+        _choiseItem.gameObject.SetActive(false);
     }
 
 
@@ -23,21 +32,29 @@ public class Gacha : MonoBehaviour
         int coinCount = _tryCoinCount * count;
         if (coinCount > PlayerDataManager.Instance.Coin)
         {
-            MassegeWindow.Show("돈이 부족합니다");
+            _massegeWindow.Show("돈이 부족합니다");
             return;
         }
+
+        OnStartGachaEvent?.Invoke();
+
+        List<UnitDataSO> dataSOs = new List<UnitDataSO>();
         
         for (int i = 0; i < count; i++)
         {
             UnitDataSO unitData = _manager.Gacha();
             Debug.Log(unitData);
             AddListUnit(unitData);
+            dataSOs.Add(unitData);
         }
-
-        PlayerDataManager.Instance.haveUnit.ForEach(unit => Debug.Log($"리스트 : { unit}"));
-
+        OnChoiseUnitCheckEvent?.Invoke(dataSOs);
         PlayerDataManager.Instance.RemoveCoin(coinCount);
+        
     }
+
+
+
+    
 
     private void AddListUnit(UnitDataSO unitData)
     {
