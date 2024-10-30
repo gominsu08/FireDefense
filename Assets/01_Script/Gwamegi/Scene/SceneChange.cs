ee04585a44e62fa4897e11bf5d5cf9d8051909d1
gominsu08 <minsu080618@gmail.com>
2024-10-31 00:50:36 +0900

    fix : gacha

---
+++ Assets/01_Script/Gwamegi/Scene/SceneChange.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    [SerializeField] private string _sceneName;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
--- Assets/01_Script/Gwamegi/Store/ChoiseItem.cs
+++ Assets/01_Script/Gwamegi/Store/ChoiseItem.cs
@@ -1,6 +1,9 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class ChoiseItem : MonoBehaviour
@@ -8,7 +11,10 @@ public class ChoiseItem : MonoBehaviour
     [SerializeField] private Gacha _gacha;
     [SerializeField] private Image _itemImage;
     [SerializeField] private RectTransform _1Panel, _2Panel;
+    [SerializeField] private Light2D lights;
     public bool isSkip { get; private set; }
+    public bool isUnitCheck { get; private set; }
+    private int _cardCreatCount;
 
     private List<GameObject> ItemList = new List<GameObject>();
 
@@ -30,39 +36,64 @@ public class ChoiseItem : MonoBehaviour
         {
             Image image = _itemImage;
             image.color = list[i].rarityColor;
+            Sprite sprite = list[i].agentSprite;
 
-            if (i < 5) { ItemList.Add(Instantiate(image, _1Panel).gameObject); }
-            else { ItemList.Add(Instantiate(image, _2Panel).gameObject); }
+            if (i < 5)
+            {
+                CreatUnitCard(image, sprite, _1Panel);
+            }
+            else
+            {
+                CreatUnitCard(image, sprite, _2Panel);
+            }
 
             yield return new WaitForSeconds(0.35f);
         }
+        isUnitCheck = true;
 
-        StartCoroutine(UnitChecker(list));
+
     }
 
-    private IEnumerator UnitChecker(List<UnitDataSO> list)
+    private void CreatUnitCard(Image image, Sprite sprite, RectTransform creatPanel)
     {
-        for (int i = 0; i < ItemList.Count; i++)
+        Image unitCard = Instantiate(image, creatPanel);
+        unitCard.AddComponent<Button>().onClick.AddListener(() =>
         {
-            if (ItemList[i].TryGetComponent(out Image imgae))
+            if (isUnitCheck)
             {
-                while (true)
+                //unitCard.transform.DORotate(new Vector3(0, 180, 0), 0.5f).OnComplete(() =>
+                //{
+                //    unitCard.sprite = sprite;
+                //    unitCard.color = Color.white;
+                //    _cardCreatCount++;
+                //    unitCard.GetComponent<Button>().enabled = false;
+                //});
+                unitCard.GetComponent<Button>().enabled = false;
+                unitCard.transform.DOScaleX(0, 0.25f).OnComplete(() =>
                 {
-                    yield return null;
-                    if (Input.GetMouseButtonDown(0))
+                    unitCard.sprite = sprite;
+                    unitCard.color = Color.white;
+                    unitCard.transform.DOScaleX(1, 0.25f).OnComplete(() =>
                     {
-                        imgae.sprite = list[i].unitSprite;
-                        Debug.Log("Ming");
-                        break;
-                    }
-                }
+                        _cardCreatCount++;
+                    });
+                });
+
             }
-        }
-        isSkip = true;
+        });
+
+        ItemList.Add(unitCard.gameObject);
     }
+
+
 
     private void Update()
     {
+        if (_cardCreatCount >= 10)
+        {
+            isSkip = true;
+        }
+
         if (Input.GetMouseButtonDown(0) && isSkip)
         {
             ItemReset();
@@ -74,6 +105,8 @@ public class ChoiseItem : MonoBehaviour
     {
         gameObject.SetActive(true);
         isSkip = false;
+        _cardCreatCount = 0;
+        isUnitCheck = false;
     }
 
     private void ItemReset()
